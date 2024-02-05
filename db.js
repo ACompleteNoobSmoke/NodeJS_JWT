56f8a123e0c8d87fa04e2831ba2679f876d2e80e
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-02-04 19:10:38 -0600

    Did Validation

---
+++ db.js
@@ -0,0 +1,8 @@
+const users = [
+    {
+        "email": "AfricanKing@gmail.com",
+        "password": "1234567"
+    }
+]
+
+module.exports = users;
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const {check} = require("express-validator");
+const {check, validationResult} = require("express-validator");
+const users = require("../db");
 
-router.post("/signup", (req, res) => {
-    const {password, email} = req.body;
-    console.log(password, email);
+router.post("/signup", [
+    check("email", "Please provide a valid email").isEmail(),
+     check("password", "Please provide a password that is greater than 5 characters").isLength({
+    min: 6
+})], (req, res) => {
+    const reqPassword = req.body.password;
+    const reqEmail = req.body.email;
+
+    // Validated the input
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).send({
+        errors: errors.array()
+    })
+
+    // Validated if user doesn't already exist
+    let user = users.find(({email, password}) => email === reqEmail && password === reqPassword);
+    if(user) return res.status(400).send({
+        "errors": [
+            {
+                "msg": "This user already exists!"
+            }
+        ]
+    })
     res.send("Auth Route Working");
 })
 
